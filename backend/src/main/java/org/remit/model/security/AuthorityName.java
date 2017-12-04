5bbffabf34e428407c1d8709cb2815828b5036bf
nirojans <Nirospass1>
2017-12-04 18:34:32 +0530

    adding initial jwt security code

---
+++ backend/src/main/java/org/remit/model/security/AuthorityName.java
@@ -0,0 +1,5 @@
+package org.remit.model.security;
+
+public enum AuthorityName {
+    ROLE_USER, ROLE_ADMIN
+}
---
+++ backend/src/main/java/org/remit/security/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package org.remit.security.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.remit.model.security.User;
+
+/**
+ * Created by stephan on 20.03.16.
+ */
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
